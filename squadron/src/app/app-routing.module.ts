792187ce41fba44937b59310120ebf8c80d2e1d9
Paul Hewitt <thephewitt@gmail.com>
2020-03-23 09:25:43 -0600

    feat(app): downgrading devkit-build, adding routing

--- squadron/src/app/app-routing.module.ts
+++ squadron/src/app/app-routing.module.ts
@@ -1,10 +1,42 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './home/home.component';
+import { DemoComponent } from './demo/demo.component';
+import { TipsComponent } from './tips/tips.component';
+import { PhoneComponent } from './phone/phone.component';
+import { AdminComponent } from './admin/admin.component';
+
+const routes: Routes = [
+
+  {
+    path: '', component: HomeComponent,
+  },
+  {
+    path: 'demo', component: DemoComponent,
+  },
+  {
+    path: 'tips', component: TipsComponent,
+  },
+  {
+    path: 'phone', component: PhoneComponent,
+  },
+  {
+    path: 'admin', component: AdminComponent,
+  },
+  {
+    path: '**',
+    redirectTo: '',
+    pathMatch: 'full'
+  }
+  ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    anchorScrolling: 'enabled',
+    scrollPositionRestoration: 'enabled',
+    onSameUrlNavigation: 'reload'
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
